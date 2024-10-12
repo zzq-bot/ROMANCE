5946971854e3ddb8a5fdad96547c5b2c10732fe6
Ziqian Zhang <475731070@qq.com>
2024-10-12 13:01:10 +0800

    Update __init__.py

--- src_back/my_run/__init__.py
+++ src_back/my_run/__init__.py
@@ -5,12 +5,3 @@ REGISTRY["robust"] = run_robust
 
 from .run_attack import run_attack
 REGISTRY["attack"] = run_attack
-
-from .run_robust_na import run_robust_na
-REGISTRY["robust_na"] = run_robust_na
-
-from .run_attack_na import run_attack_na
-REGISTRY["attack_na"] = run_attack_na
-
-from .eval_na import run_eval_na
-REGISTRY["eval_na"] = run_eval_na
